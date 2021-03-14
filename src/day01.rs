e4fe6f51df0106653700067f6213d20e1d7f80ac
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-03-14 09:02:57 +0100

    Rust experiments

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,41 @@
-pub fn part1fun() {
-    println!("Hello world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn expected() -> (i32, i32) {
+    (987339, 259521570)
+}
+
+pub fn parse_input() -> Result<Vec<i32>, std::io::Error> {
+    Ok(BufReader::new(File::open("inputs/2020/input01.txt")?)
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect())
+}
+
+pub fn part1(input: &Vec<i32>) -> Result<i32, &'static str> {
+    for &x in input.iter() {
+        for &y in input.iter() {
+            if x + y == 2020 {
+                return Ok(x * y);
+            }
+        }
+    }
+    return Err("no result found");
+}
+
+pub fn part2(input: &Vec<i32>) -> Result<i32, &'static str> {
+    let smallest = input.iter().min().unwrap();
+    for &x in input.iter() {
+        for &y in input.iter() {
+            if x == y || x + y + smallest >= 2020 {
+                continue;
+            }
+            for &z in input.iter() {
+                if x + y + z == 2020 {
+                    return Ok(x * y * z);
+                }
+            }
+        }
+    }
+    return Err("no result found");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod day01;
 
 fn main() {
-    day01::part1fun();
-    day01::part1fun();
+    let now = std::time::Instant::now();
+    match day01::parse_input() {
+        Ok(input) => {
+            let r1 = day01::part1(&input);
+            let r2 = day01::part2(&input);
+            println!("Time: {} s", now.elapsed().as_secs_f64());
+
+            let actual = (r1.unwrap(), r2.unwrap());
+            assert_eq!(actual, day01::expected());
+        }
+        Err(what) => {
+            panic!(what);
+        }
+    }
 }
+
+// fn run_puzzle<T>(puzzle: T) {}
